43d772e0e00bfa1f5b92e6b8361c899f95a5fa2c
wolfgunblood <jalajdorai1@gmail.com>
2023-05-06 10:54:24 +0530

    Toggle darkmode in reducer

--- src/reducers/reducer.tsx
+++ src/reducers/reducer.tsx
@@ -1,6 +1,18 @@
+import sampleStore from "../utils/samples/sampleStore";
 
-const reducer = (state = "", action: any) => {
+const reducer = (state = sampleStore, action: any) => {
 
+    switch (action.type) {
+        case 'darkmode/TOGGLE':
+            // eslint-disable-next-line no-case-declarations
+            const darkmode = {
+                ...state,
+                darkmode: !state.darkmode,
+            };
+            return darkmode;
+        default:
+            return state;
+    }
 }
 
 export default reducer;
