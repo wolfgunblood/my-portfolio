808a079f7865ca0d53d2bc96ecafb76969eb00d5
wolfgunblood <jalajdorai1@gmail.com>
2023-05-08 16:14:42 +0530

    Added Contact

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { store } from '../../App';
+import { motion } from 'framer-motion';
+
+
 
 const Contact = () => {
+
+  const [state, dispatch] = useContext(store);
   return (
     <div>Contact</div>
   )
