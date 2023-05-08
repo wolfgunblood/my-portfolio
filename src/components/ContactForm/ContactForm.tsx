d826f3d7fb65809ee38c9cc9d781f4e0f59ce93e
wolfgunblood <jalajdorai1@gmail.com>
2023-05-08 19:08:11 +0530

    Modified Contact Form

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useContext, useRef } from 'react';
+import './ContactForm.scss';
+import { store } from '../../App';
 
 const ContactForm = () => {
+  const [ state, dispatch ] = useContext(store);
+
   return (
-    <div>ContactForm</div>
+    <form 
+      onSubmit={() => {}}
+      className={`contact-form 
+        ${state.darkMode ? 'dark-card' : 'light-card'}
+        ${state.darkMode ? 'dark-shadow' : 'light-shadow'}
+        `}
+    >
+      <div  className='input-container'>
+        <input 
+          type="text"
+          name='user_name'
+          placeholder='Your Name'
+          className={`field data ${state.darkMode ? 'dark-field' : 'light-field'}`}
+          required 
+        />
+        <input 
+          type="email"
+          name='user_email'
+          placeholder='Your Email'
+          className={`field data ${state.darkMode ? 'dark-field' : 'light-field'}`}
+          required 
+          />
+      </div>
+      <textarea 
+        name="message" 
+        placeholder="Your Message for me"  
+        className={`field ${state.darkMode ? 'dark-field' : 'light-field'}`}
+        required 
+      />
+      <input 
+        type="submit"
+        value={"Submit"}
+        className='submit' 
+      />
+    </form>
   )
 }
 
