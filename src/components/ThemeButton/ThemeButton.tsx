2ef0f782054d3f6fc7cf459e9f4ce54038c3c1d4
wolfgunblood <jalajdorai1@gmail.com>
2023-05-22 21:35:56 +0530

    Added toast for Theme Button

--- src/components/ThemeButton/ThemeButton.tsx
+++ src/components/ThemeButton/ThemeButton.tsx
@@ -2,20 +2,46 @@ import { useContext } from 'react';
 import './ThemeButton.scss'
 import { motion } from 'framer-motion';
 import { store } from '../../App';
+import toast, { Toaster } from "react-hot-toast";
 
 const ThemeButton = () => {
-    const [ state, dispatch ] = useContext(store);
+    const [state, dispatch] = useContext(store);
     //   const { theme } = state;
 
     const handleTheme = () => {
         dispatch({ type: 'darkmode/TOGGLE' });
+        if (state.darkMode){
+            toast('Hello Lightness!',
+                {
+                    icon: '☀',
+                    position: "top-left",
+                    style: {
+                        borderRadius: '10px',
+                        background: '#fff',
+                        color: '#333',
+                    },
+                });
+            }
+        else {
+            toast('Hello Darkness!',
+                {
+                    icon: '👏',
+                    position: "top-left",
+                    style: {
+                        borderRadius: '10px',
+                        background: '#333',
+                        color: '#fff',
+                    },
+                }
+            );
+        }
     }
 
     return (
         <motion.button
             initial={{ y: 150 }}
             animate={{ y: 0 }}
-            transition={{ type: 'spring',duration: 0.2 }}
+            transition={{ type: 'spring', duration: 0.2 }}
             className={`theme-button
                 ${state.darkmode ? 'dark-theme-button' : 'light-theme-button'} 
             `}
