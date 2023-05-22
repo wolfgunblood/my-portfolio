d86293f69b42ee0b47eaa7b7cc9b1f05c2dd2455
wolfgunblood <jalajdorai1@gmail.com>
2023-05-22 20:45:38 +0530

    Fixed issue of lag in skills

--- src/components/SkillTab/SkillTab.tsx
+++ src/components/SkillTab/SkillTab.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { store } from '../../App';
 import './SkillTab.scss';
-import { motion } from 'framer-motion';
+import { motion, Variants } from 'framer-motion';
 // import Imgae from '../Image/Imgae';
 import Image from '../Image/Image';
 import {
@@ -25,70 +25,93 @@ import {
   VSCode,
 } from '../../assets';
 
-const SkillTab = ({topic} :any) => {
+const SkillTab = ({ topic }: any) => {
 
-  const [ state, _dispatch ] = useContext(store);
+  const [state, _dispatch] = useContext(store);
+
+  const skillVariants: Variants = {
+    offscreen: {
+      y: 300
+    },
+    onscreen: {
+      y: 50,
+      // rotate: -10,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.8
+      }
+    }
+  };
+
 
   return (
     <motion.div
-      className={`skill-tab-container ${state.darkMode ? "dark-card": "light-card"}`}
-      initial={{ scale: 0 }}
-      whileInView={{ scale: 1 }}
-      viewport={{ once: true }}
-      transition={{ duration: 0.5,type: "spring",bounce: 0.3,delay: topic === "libraries" ? 0.35 : topic === "prototyping" ? 0.35 : topic === "code" ? 0.65 : 0.05 }}
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: true, amount: 0.8 }}
     >
-      <h3 className={`head ${state.darkMode ? "white": ""}`}>
-        {topic === "languages" ? "Languages" : null}
-        {topic === "libraries" ? "Libraries" : null}
-        {topic === "frameworks" ? "Frameworks" : null}
-        {topic === "collaborative" ? "Collaborative Work" : null}
-        {topic === "prototyping" ? "Prototyping" : null}
-        {topic === "code" ? "Code Editor" : null}
-      </h3>
-      {topic === "languages" ? (
-        <div className='skill-tab-list languages'>
-          <Image source={Typescript} name="Typescript" />
-          <Image source={Javascript} name="JavaScript" />
-          <Image source={HTML} name="HTML" />
-          <Image source={CSS} name="CSS" />
-          <Image source={SASS} name="SASS" />
-          <Image source={Python} name="Python" />
-        </div>
-      ) : null}
-      {topic === "frameworks" ? (
-        <div  className='skill-tab-list'>
-          <Image source={React1} name="React" />
-          <Image source={state.darkMode ? Next2 : Next} name="Next.js" /> 
-          <Image source={Tailwind} name="Tailwind" />
+      <motion.div
+        className={`skill-tab-container ${state.darkMode ? "dark-card" : "light-card"}`}
+        // initial={{ scale: 0 }}
+        // whileInView={{ scale: 1 }}
+        // viewport={{ once: true }}
+        // transition={{ duration: 0.5, type: "spring", bounce: 0.3, delay: topic === "libraries" ? 0.35 : topic === "prototyping" ? 0.35 : topic === "code" ? 0.65 : 0.05 }}
+        variants={skillVariants}
+      >
+        <h3 className={`head ${state.darkMode ? "white" : ""}`}>
+          {topic === "languages" ? "Languages" : null}
+          {topic === "libraries" ? "Libraries" : null}
+          {topic === "frameworks" ? "Frameworks" : null}
+          {topic === "collaborative" ? "Collaborative Work" : null}
+          {topic === "prototyping" ? "Prototyping" : null}
+          {topic === "code" ? "Code Editor" : null}
+        </h3>
+        {topic === "languages" ? (
+          <div className='skill-tab-list languages'>
+            <Image source={Typescript} name="Typescript" />
+            <Image source={Javascript} name="JavaScript" />
+            <Image source={HTML} name="HTML" />
+            <Image source={CSS} name="CSS" />
+            <Image source={SASS} name="SASS" />
+            <Image source={Python} name="Python" />
+          </div>
+        ) : null}
+        {topic === "frameworks" ? (
+          <div className='skill-tab-list'>
+            <Image source={React1} name="React" />
+            <Image source={state.darkMode ? Next2 : Next} name="Next.js" />
+            <Image source={Tailwind} name="Tailwind" />
 
-        </div>
-      ): null}
-      {topic === "libraries" ? (
-        <div className='skill-tab-list'>
-          <Image source={Redux1} name="Redux" />
-          <Image source={FramerMotion} name="Framer Motion" />
-          <Image source={ChakraUI} name="Chakra UI" /> 
+          </div>
+        ) : null}
+        {topic === "libraries" ? (
+          <div className='skill-tab-list'>
+            <Image source={Redux1} name="Redux" />
+            <Image source={FramerMotion} name="Framer Motion" />
+            <Image source={ChakraUI} name="Chakra UI" />
 
-        </div>
-      ) : null}
-      {topic === "collaborative" ? (
-        <div className='skill-tab-list'>
-          <Image source={Git} name="Git" /> 
-          <Image source={Github} name="Github" />  
+          </div>
+        ) : null}
+        {topic === "collaborative" ? (
+          <div className='skill-tab-list'>
+            <Image source={Git} name="Git" />
+            <Image source={Github} name="Github" />
 
-        </div>
-      ): null}
-      {topic === "prototyping" ? (
-        <div className='skill-tab-list'> 
-          <Image source={Figma} name="Figma" />
-          <Image source={Photoshop} name="Photoshop" /> 
-        </div>
-      ): null}
-      {topic === "code" ? (
-        <div className='skill-tab-list'>
-          <Image source={VSCode} name="VS Code" /> 
-        </div>
-      ): null}
+          </div>
+        ) : null}
+        {topic === "prototyping" ? (
+          <div className='skill-tab-list'>
+            <Image source={Figma} name="Figma" />
+            <Image source={Photoshop} name="Photoshop" />
+          </div>
+        ) : null}
+        {topic === "code" ? (
+          <div className='skill-tab-list'>
+            <Image source={VSCode} name="VS Code" />
+          </div>
+        ) : null}
+      </motion.div>
     </motion.div>
   )
 }
