cb2bc5c2623e2de4d396d405b151aed3f31a7417
wolfgunblood <jalajdorai1@gmail.com>
2023-05-29 20:32:52 +0530

    bug Fix of base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mu-portfolio',
+  base: '/my-portfolio',
   plugins: [
     react(),
     svgr(),
