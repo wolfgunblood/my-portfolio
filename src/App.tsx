4973c501813ecbbda7c312b16966e4b88913e8e3
wolfgunblood <jalajdorai1@gmail.com>
2023-05-29 20:35:08 +0530

    added script

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Skills from './components/Skills/Skills';
 import Contact from './components/Contact/Contact';
-import toast, { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const store = createContext<any>(null);
