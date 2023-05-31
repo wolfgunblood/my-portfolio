8fdc6bc9503572abf9c06a5811c06d9651c051a8
wolfgunblood <jalajdorai1@gmail.com>
2023-05-31 11:48:34 +0530

    Changed gh pages config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-portfolio',
+  base: '/portfolio',
   plugins: [
     react(),
     svgr(),
