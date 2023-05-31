25dc78aeaca28ebe99d9a27a70ff96c7985aeb70
wolfgunblood <jalajdorai1@gmail.com>
2023-05-31 21:05:29 +0530

    Addes Badges and fixed height overflow of badges

--- src/components/Badges/Badges.tsx
+++ src/components/Badges/Badges.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import "./Badge.scss";
+import { SiTypescript, SiRedux } from 'react-icons/si';
+import { FaReact, FaSass, FaGitAlt } from 'react-icons/fa';
+import { TbBrandFramerMotion } from 'react-icons/tb';
+import { AiOutlineGithub } from 'react-icons/ai';
 
 const Badges = () => {
-  return (
-    <div>Badges</div>
-  )
+    return (
+        <div className='badges-container'>
+            <button className='badge-btn'>
+                <SiTypescript />
+                TypeScript
+            </button>
+            <button className='badge-btn'>
+                <FaReact />
+                React
+            </button>
+            <button className='badge-btn'>
+                <FaSass />
+                Sass
+            </button>
+            <button className='badge-btn'>
+                <TbBrandFramerMotion />
+                Framer Motion
+            </button>
+            <button className='badge-btn'>
+                <SiRedux />
+                Redux
+            </button>
+            <button className='badge-btn'>
+                <FaGitAlt />
+                Git
+            </button>
+            <button className='badge-btn'>
+                <AiOutlineGithub />
+                Github
+            </button>
+        </div>
+    )
 }
 
 export default Badges
