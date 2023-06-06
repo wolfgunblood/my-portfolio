227843b96bfe5e2cbd57d04a716e2914781e8800
wolfgunblood <jalajdorai1@gmail.com>
2023-06-06 19:26:07 +0530

    Added Download Functionality in Resume Button

--- src/components/ResumeButton/ResumeButton.tsx
+++ src/components/ResumeButton/ResumeButton.tsx
@@ -3,9 +3,9 @@ import { useContext, useState } from 'react';
 import { motion } from 'framer-motion';
 import { store } from '../../App';
 import toast from "react-hot-toast";
-// import { GrDocumentDownload } from 'react-icons/gr';
 import { IoDocumentTextOutline } from 'react-icons/io5';
 import { IconContext } from "react-icons";
+import Resume from "../../assets/Resume/jalajCV.pdf"
 
 const ResumeButton = () => {
     const [state, dispatch] = useContext(store);
@@ -13,6 +13,12 @@ const ResumeButton = () => {
     const [hovered, setHovered] = useState(false);
 
     const handleDownload = () => {
+
+        const link = document.createElement('a');
+        link.href = Resume;
+        link.download = 'Resume.pdf';
+        link.click();
+
         if (!state.darkMode) {
             toast.success('Download Successful',
                 {
